ce4d087482068d0c684a7cba3d0696bddf6dd7d5
faeyzaaa <faeyzasafaid@student.ub.ac.id>
2025-11-10 08:09:42 +0700

    Motor.java

--- src/rental/Motor.java
+++ src/rental/Motor.java
@@ -2,12 +2,24 @@ package rental;
 
 public class Motor extends Kendaraan {
 
+    private boolean helmDisediakan;
+
     public Motor(String id, String nama, double biayaHarian, boolean tersedia) {
         super(id, nama, biayaHarian, tersedia);
+        this.helmDisediakan = true; 
     }
 
     @Override
     protected double hitungBiayaTambahan() {
+        
         return 20_000;
+    }
+
+    @Override
+    public void tampilkanDetail() {
+        System.out.println("Jenis: Motor");
+        super.tampilkanDetail();
+        System.out.println("Helm disediakan: " + (helmDisediakan ? "Ya" : "Tidak"));
+        System.out.println("--------------------------");
     }
 }
